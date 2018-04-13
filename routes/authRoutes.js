51624180bd07c7f11cb43dc165adab5ad3eafdd2
William del Rosario <williamdelrosario@yahoo.com>
2018-04-13 12:47:21 +0200

    Logging out users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,11 @@ module.exports = (app) => {
 
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        req.send(req.user);
+    });
+
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
     });
