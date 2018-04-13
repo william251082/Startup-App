b9c76decafd39bd5bf55a9c28fddf18dd0663b3e
William del Rosario <williamdelrosario@yahoo.com>
2018-04-13 20:59:20 +0200

    Header content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,11 +6,11 @@ class Header extends Component
     renderContent() {
         switch (this.props.auth) {
             case null:
-                return 'Still deciding';
+                return;
             case false:
-                return 'im logged out';
+                return <li><a href="/auth/google">Login With Google</a></li>;
             default:
-                return 'im logged in';
+                return <li><a>Logout</a></li>;
         }
     }
 
