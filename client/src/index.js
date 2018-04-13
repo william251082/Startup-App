23244c70f5ae44b60a08dc369e7802b26149b32a
William del Rosario <williamdelrosario@yahoo.com>
2018-04-13 19:57:10 +0200

    Additional proxy rules

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reducThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
