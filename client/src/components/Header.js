76d02f7800a5473461ea1980ba2bd53fef745c06
William del Rosario <williamdelrosario@yahoo.com>
2018-04-13 19:43:58 +0200

    Header design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,16 @@ class Header extends Component
 {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">
+                        Emaily
+                    </a>
+                    <ul className="right">
+                        <li><a>Login With Google</a></li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
