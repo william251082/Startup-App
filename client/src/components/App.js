74924641ada46a4ad627848f46a4ef58312f02c9
William del Rosario <williamdelrosario@yahoo.com>
2018-04-13 18:57:55 +0200

    Always visible components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,8 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" component={Landing}></Route>
+                    <Route exact path="/" component={Landing}></Route>
+                    <Route path="/surveys" component={Dashboard}></Route>
                 </div>
             </BrowserRouter>
         </div>
