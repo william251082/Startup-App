108a26150e660b0ca03f4ed1f7492393dbc55188
William del Rosario <williamdelrosario@yahoo.com>
2018-04-15 17:02:51 +0200

    Mailer constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -4,7 +4,14 @@ const keys = require('../config/keys');
 
 class Mailer extends helper.Mail
 {
+    constructor({ subject, reciepients }, content) {
+        super();
 
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('test/html', content);
+        this.recipients = this.formatAddresses(reciepients);
+    }
 }
 
 module.exports = Mailer;
