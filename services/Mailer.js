7a66d62524eea6236093d3ca7d9062d1d5596645
William del Rosario <williamdelrosario@yahoo.com>
2018-04-15 19:50:44 +0200

    Feedback for user feedback

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,7 +10,7 @@ class Mailer extends helper.Mail
         this.sgApi = sendgrid(keys.sendGridKey);
         this.from_email = new helper.Email('no-reply@emaily.com');
         this.subject = subject;
-        this.body = new helper.Content('test/html', content);
+        this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
 
         this.addContent(this.body);
