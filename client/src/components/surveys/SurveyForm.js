ca23cf41dac57717ea82650a0c2ae40eab72543d
William del Rosario <williamdelrosario@yahoo.com>
2018-04-15 22:31:20 +0200

    Custom fields components

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,9 +1,18 @@
 // SurveyForm shows a form for a user to add input
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component
 {
+    renderFields() {
+        return (
+            <div>
+                <Field type="text" name="title" component={SurveyField} />
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
