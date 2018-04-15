697691457212e796cc81c6ffa056dbce51daa1a9
William del Rosario <williamdelrosario@yahoo.com>
2018-04-15 20:13:59 +0200

    Navigation with the link tag

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
@@ -6,7 +7,9 @@ const Dashboard = () => {
             Dashboard
             <div className="fixed-action-btn">
                 <a className="btn-floating btn-large red">
-                    <i className="material-icons">add</i>
+                    <Link to="/surveys/new" className="btn-floating btn-large red">
+                        <i className="material-icons">add</i>
+                    </Link>
                 </a>
             </div>
         </div>
