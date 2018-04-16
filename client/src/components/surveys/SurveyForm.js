562400e13d76137e7aed8cc08fa4263e1da84cd6
William del Rosario <williamdelrosario@yahoo.com>
2018-04-16 09:03:58 +0200

    Retreat to the form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -33,7 +33,7 @@ class SurveyForm extends Component
         return (
             <div>
                 <form
-                    onSubmit={this.props.handleSubmit(values => console.log(values))}
+                    onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}
                 >
                     {this.renderFields()}
                     <Link to="/surveys" className="red btn-flat white-text">
