c94a3adc569f41d6d6759e4b19cb9eaaa520838b
William del Rosario <williamdelrosario@yahoo.com>
2018-04-16 09:24:52 +0200

    Refactoring form fields

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,10 +2,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
     return (
         <div>
             <h5>Please confirm your entries</h5>
+            <div>
+                <div>
+                    <label>Survey Title</label>
+                    <div>{formValues.title}</div>
+                </div>
+            </div>
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
