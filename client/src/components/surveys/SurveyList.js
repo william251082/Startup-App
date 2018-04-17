d0592ddd2e02a05bae3ad3b7b9b870fed004efc8
William del Rosario <williamdelrosario@yahoo.com>
2018-04-17 10:20:51 +0200

    Rendering and recversing the list of surveys list

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,9 +8,34 @@ class SurveyList  extends Component
         this.props.fetchSurveys();
     }
 
+    renderSurveys() {
+        return this.props.surveys.reverse().map(survey => {
+            return (
+                <div className="card darken-1" key={survey.id}>
+                    <div className="card-content">
+                        <span className="card-title">
+                            <p>
+                                {survey.body}
+                            </p>
+                            <p className="right">
+                                Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+                            </p>
+                        </span>
+                    </div>
+                    <div className="card-action">
+                        <a>Yes: {survey.yes}</a>
+                        <a>No: {survey.no}</a>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
         return (
-            <div>SurveyList</div>
+            <div>
+                {this.renderSurveys()}
+            </div>
         );
     }
 }
--- queriesForCli.js
+++ queriesForCli.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const keys = require('./config/keys');
-require('./models/User');
-require('./models/Survey');
-require('./services/passport');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
-
-const Survey = mongoose.model('surveys');
-
-Survey.find({}).then(console.log);
-
-Survey.find({ title: 'FINAL TEST!'}).then(console.log);
-
-Survey.find({ yes: 0 }).then(console.log);
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cookieSession = require('cookie-session');
+// const passport = require('passport');
+// const bodyParser = require('body-parser');
+// const keys = require('./config/keys');
+// require('./models/User');
+// require('./models/Survey');
+// require('./services/passport');
+//
+// mongoose.Promise = global.Promise;
+// mongoose.connect(keys.mongoURI);
+//
+// const Survey = mongoose.model('surveys');
+//
+// Survey.find({}).then(console.log);
+//
+// Survey.find({ title: 'FINAL TEST!'}).then(console.log);
+//
+// Survey.find({ yes: 0 }).then(console.log);
 
 
 // "webhook": "./sendgrid_webhook.sh"
