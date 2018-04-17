df391c2590b090ce7534d4394e9d151788fb930f
William del Rosario <williamdelrosario@yahoo.com>
2018-04-17 09:52:02 +0200

    Wiring react up to redux

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Surveylist from './surveys/SurveyList';
 
 const Dashboard = () => {
     return (
         <div>
-            Dashboard
+            <Surveylist />
             <div className="fixed-action-btn">
                     <Link to="/surveys/new" className="btn-floating btn-large red">
                         <i className="material-icons">add</i>
